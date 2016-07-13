ce168381a620ff8c85cd962f209cbb5bb39a667d
Martin <martin.grzeslowski@gmail.com>
2016-07-13 10:34:43 +0200

    Update konfiguracji

--- config/config.js
+++ config/config.js
@@ -11,42 +11,51 @@ var config = {
 		},
 		{
 			module: 'clock',
-			position: 'top_left'
+			position: 'top_right'
+			config: {
+				showPeriodUpper: true
+			}
 		},
 		{
 			module: 'calendar',
-			header: 'US Holidays',
-			position: 'top_left',
+			header: 'Kalendarz',
+			position: 'top_right',
 			config: {
+				maximumEntries: 25,
+				
 				calendars: [
 					{
 						symbol: 'calendar-check-o ',
-						url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
+						url: 'https://calendar.google.com/calendar/ical/martin.grzeslowski%40gmail.com/private-18a95defd98f0fc2b552925b0311efa7/basic.ics'
+					},
+					{
+						symbol: 'tripadvisor ',
+						url: 'https://calendar.google.com/calendar/ical/1jvicjgqdfl9pq65mj2a1b9uv8%40group.calendar.google.com/private-c4b8729cec6a20cf00d727ec247df304/basic.ics',
+						repeatingCountTitle: 'Airbnb'
+					},
+					{
+						symbol: 'cogs ',
+						url: 'https://calendar.google.com/calendar/ical/uu3stjobudouuictf8pjflkqfs%40group.calendar.google.com/private-9ffcd6643240256a444d7c698c44e249/basic.ics'
+						repeatingCountTitle: 'Praca'
 					}
 				]
 			}
 		},
 		{
-			module: 'compliments',
-			position: 'lower_third'
-		},
-		{
 			module: 'currentweather',
-			position: 'top_right',
+			position: 'top_left',
 			config: {
-				location: 'New York',
-				locationID: '',  //ID from bulk.openweather.org/sample/
-				appid: 'YOUR_OPENWEATHER_API_KEY'
+				location: 'Wroclaw, Poland',
+				appid: '39640ff4c824892da2d2dc8afa830a4a'
 			}
 		},
 		{
 			module: 'weatherforecast',
-			position: 'top_right',
+			position: 'top_left',
 			header: 'Weather Forecast',
 			config: {
-	            location: 'New York',
-				locationID: '5128581',  //ID from bulk.openweather.org/sample/
-	            appid: 'YOUR_OPENWEATHER_API_KEY'
+	            		location: 'Wroclaw, Poland',
+	            		appid: '39640ff4c824892da2d2dc8afa830a4a'
 			}
 		},
 		{
@@ -55,8 +64,8 @@ var config = {
 			config: {
 				feeds: [
 					{
-						title: "New York Times",
-						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+						title: "Antyweb",
+						url: "http://feeds.feedburner.com/Antyweb?format=xml"
 					}
 				],
 				showSourceTitle: true,
